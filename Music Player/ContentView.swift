3103de4b5ed4b39ffc4d0c15d469e83e42ac62fd
edlyn <edlyn.liew@outlook.com>
2020-08-12 00:01:06 +1000

    Initial Commit

---
+++ Music Player/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Music Player
+//
+//  Created by Edlyn Liew  on 12/8/20.
+//  Copyright © 2020 Edlyn Liew . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
