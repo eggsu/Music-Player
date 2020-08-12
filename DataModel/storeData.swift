c2025f45bf3cf9fb2bf966fdb70c327fdda5fc54
edlyn <edlyn.liew@outlook.com>
2020-08-12 21:26:55 +1000

    added in music player buttons & connection to the media player. -Simple UI so far

---
+++ DataModel/storeData.swift
@@ -0,0 +1,14 @@
+//
+//  storeData.swift
+//  Music Player
+//
+//  Created by Edlyn Liew  on 12/8/20.
+//  Copyright © 2020 Edlyn Liew . All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class MusicPLayer: Object{
+    
+}
---
+++ Music Player/GenreButtonScreen.swift
@@ -0,0 +1,54 @@
+//
+//  GenreButtonScreen.swift
+//  Music Player
+//
+//  Created by Edlyn Liew  on 12/8/20.
+//  Copyright © 2020 Edlyn Liew . All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+
+class GenreButtonScreen: UIViewController {
+
+    //create music player variable
+    //once close app, the music player will stop
+    var musicPlayer=MPMusicPlayerController.applicationMusicPlayer
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+        
+        //! force wrap it
+        playGenre(genre: sender.currentTitle!)
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        musicPlayer.stop()
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        musicPlayer.skipToNextItem()
+    }
+    
+    func playGenre(genre: String){
+        musicPlayer.stop()
+        
+        //create the query
+        let query=MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+     
+        //applies the filter onto the song  you are going to get
+        query.addFilterPredicate(predicate)
+        
+        //get back chosen genre
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+    
+    }
+}
